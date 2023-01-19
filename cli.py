7fcbfe1adc3533a26c5e6a209695d63ed8e85c9d
Cameron Ratchford <camratchford@gmail.com>
2023-01-19 12:58:45 -0700

    Fixed missing parameter validation in cli.py

--- cli.py
+++ cli.py
@@ -1,7 +1,6 @@
 import sys
 
 import click
-from click.testing import CliRunner
 from ez_temp.config import config, configure_default_app_directory
 from ez_temp.__main__ import main
 
@@ -10,6 +9,7 @@ from ez_temp.__main__ import main
     "-c",
     "--config-file",
     envvar="EZT_CONF",
+    type=click.Path(file_okay=True, dir_okay=False, exists=True),
     default=configure_default_app_directory(),
     help="The location of the yaml configuration file",
     show_default=True,
@@ -17,9 +17,14 @@ from ez_temp.__main__ import main
 @click.option(
     "-v",
     "--var-file",
+    type=click.Path(file_okay=True, dir_okay=False, exists=True),
     help="The relative or absolute path of the variable file to fill the template with",
 )
-@click.argument("templates", nargs=-1)
+@click.argument(
+    "templates", 
+    nargs=-1,
+    type=click.Path(file_okay=True, dir_okay=False, exists=True),
+    )
 def run(config_file: str, var_file: str, templates: list):
     config.configure_from_file(config_file)
     main(config, var_file, [t for t in templates])
--- ez_temp/cli.py
+++ ez_temp/cli.py
@@ -4,11 +4,13 @@ from ez_temp.config import config, configure_default_app_directory
 from ez_temp.__main__ import main
 
 
+
 @click.command(context_settings={"ignore_unknown_options": True})
 @click.option(
     "-c",
     "--config-file",
     envvar="EZT_CONF",
+    type=click.Path(file_okay=True, dir_okay=False, exists=True),
     default=configure_default_app_directory(),
     help="The location of the yaml configuration file",
     show_default=True,
@@ -16,10 +18,15 @@ from ez_temp.__main__ import main
 @click.option(
     "-v",
     "--var-file",
+    type=click.Path(file_okay=True, dir_okay=False, exists=True),
     help="The relative or absolute path of the variable file to fill the template with",
 )
-@click.argument("templates", nargs=-1)
+@click.argument(
+    "templates", 
+    nargs=-1,
+    type=click.Path(file_okay=True, dir_okay=False, exists=True),
+    )
 def run(config_file: str, var_file: str, templates: list):
-    print("Hi!")
     config.configure_from_file(config_file)
     main(config, var_file, [t for t in templates])
+
