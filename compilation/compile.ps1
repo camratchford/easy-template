d6c56db9449c245bca0f7afb5d78058658e7e051
Cam Ratchford <camratchford@gmail.com>
2023-10-09 12:52:08 -0600

    Fixed up the windows compilation process to include an icon

--- compilation/compile.ps1
+++ compilation/compile.ps1
@@ -1,4 +1,6 @@
 # # PWD should be the same directory is this file resides
+$PreviousPWD = Get-Location
+
 
 $ScriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 $RepoRoot = Split-Path -Parent $ScriptPath
@@ -7,27 +9,29 @@ $RepoRoot = Split-Path -Parent $ScriptPath
 Set-Location $ScriptPath
 
 $DistPath = Join-Path -Path $ScriptPath -ChildPath "dist"
-$BuildPath = Join-Path $ScriptPath -ChildPath .\build -ErrorAction SilentlyContinue
-$SpecPath = Join-Path $ScriptPath -ChildPath .\ezt.spec -ErrorAction SilentlyContinue
+$BuildPath = Join-Path $ScriptPath -ChildPath build -ErrorAction SilentlyContinue
+$IconPath = Join-Path $ScriptPath -ChildPath media\ezt.png -ErrorAction SilentlyContinue
+$SpecPath = Join-Path $ScriptPath -ChildPath ezt.spec -ErrorAction SilentlyContinue
 
 
 
 
 $VENV_PATH= (Join-Path $RepoRoot -ChildPath "venv")
 
+
 if (!(Test-Path $VENV_PATH)) {
     python -m venv $VENV_PATH
+
 }
 
 $EXE_NAME="ezt"
 $INCLUDE_PATHS="../"
 $SCRIPT_FILE="../cli.py"
 Start-Process -FilePath (Join-Path -Path $VENV_PATH -ChildPath "scripts\activate")
+Start-Process -FilePath (Join-Path -Path $VENV_PATH -ChildPath "scripts\pip") -ArgumentList @("install pillow")  -Wait -NoNewWindow
 Start-Process -FilePath (Join-Path -Path $VENV_PATH -ChildPath "scripts\pip") -ArgumentList @("install -r `"$RepoRoot\ez_temp\requirements.txt`"")  -Wait -NoNewWindow
 Start-Process -FilePath (Join-Path -Path $VENV_PATH -ChildPath "scripts\pip") -ArgumentList @("install ..") -Wait -NoNewWindow
-Start-Process -FilePath (Join-Path -Path $VENV_PATH -ChildPath "scripts\pyinstaller") -ArgumentList @("-y", "--clean", "--console", "--onefile", "--name=$EXE_NAME", "--paths=$INCLUDE_PATHS", "--collect-submodules=ez_temp", $SCRIPT_FILE) -Wait -NoNewWindow
+Start-Process -FilePath (Join-Path -Path $VENV_PATH -ChildPath "scripts\pyinstaller") -ArgumentList @("-y", "--clean", "--console", "--onefile", "--icon=$IconPath", "--name=$EXE_NAME", "--paths=$INCLUDE_PATHS", "--collect-submodules=ez_temp", $SCRIPT_FILE) -Wait -NoNewWindow
 
-#.\dist\ezt.exe --help
-#
-#.\dist\ezt.exe -v "valid_vars.yml" "valid_README.md.j2"
-Copy-Item "C:\Users\Installer\git_repos\easy-template\compilation\dist\ezt.exe" "C:\ToolBox\Utilities\PSModules\PLM-Templating\1.0.0\ezt\ezt.exe" -Force
+
+Set-Location $PreviousPWD
